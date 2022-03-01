62b2acec3551c575dc32399c8f46275eb39f4d17
swindlesmccoop <swindlesmccoop@waifu.club>
2022-03-01 22:44:49 +0000

    Alphabetical organization
    Former-commit-id: e4ffea059a9a17470360a713987d557d3b9d7165

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,55 +1,55 @@
 #general aliases
+alias ":q"="exit"
+alias c="clear"
+alias enc="gpg -c --no-symkey-cache --cipher-algo AES256"
+alias flac-mp3="flac2mp3 -b 320 *.flac"
+alias gdl="gdrive download"
+alias home="cd ~"
+alias install="paru -S"
+alias p="paru -S"
+alias pms="paru -S"
+alias py3="python3"
+alias py="python"
+alias scrape="python3 ~/scripts/4chan-downloader/inb4404.py"
+alias sentra="cp *.mp3 /run/media/swindles/SENTRA/"
+alias sitedl="wget --recursive --domains swindlesmccoop.xyz --page-requisites swindlesmccoop.xyz"
+alias syyu="pacman -Syyu"
+alias timeset="date -s '[DAY] [MONTH] [YEAR] [HOURS]:[MINUTES]:[SECONDS]'"
+alias vi="vim"
 alias vi=nvim
 alias vim=nvim
 alias yay=paru
-alias flac-mp3="flac2mp3 -b 320 *.flac"
-alias sentra="cp *.mp3 /run/media/swindles/SENTRA/"
-alias home="cd ~"
-alias enc="gpg -c --no-symkey-cache --cipher-algo AES256"
-alias pms="paru -S"
-alias p="paru -S"
-alias install="paru -S"
-alias syyu="pacman -Syyu"
-alias gdl="gdrive download"
-alias ":q"="exit"
-alias timeset="date -s '[DAY] [MONTH] [YEAR] [HOURS]:[MINUTES]:[SECONDS]'"
-alias sitedl="wget --recursive --domains swindlesmccoop.xyz --page-requisites swindlesmccoop.xyz"
-alias vi="vim"
-alias c="clear"
-alias py="python"
-alias py3="python3"
-alias scrape="python3 ~/scripts/4chan-downloader/inb4404.py"
 
 #petscii type aliases
-alias mK="mkdir"
-alias pM="pacman"
 alias cL="clear"
 alias eC="echo"
+alias mK="mkdir"
+alias pM="pacman"
 
 #edit configurations
+alias pacman.conf="sudo vim /etc/pacman.conf"
+alias preview="sddm-greeter --test-mode --theme"
+alias sddm.conf="sudo vim /etc/sddm.conf"
+alias sudoers="visudo"
 alias vimrc="vim ~/.config/nvim/init.vim"
 alias zshrc="vim ~/.config/zsh/.zshrc"
-alias sudoers="visudo"
-alias pacman.conf="sudo vim /etc/pacman.conf"
-alias sddm.conf="sudo vim /etc/sddm.conf"
-alias preview="sddm-greeter --test-mode --theme"
 
 #git aliases
 alias gacap="git add . && git commit -a && git push"
 alias gc="git clone"
 
 #spellign mistaeks
-alias cleae="clear"
-alias claer="clear"
-alias clera="clear"
-alias clare="clear"
-alias claer="clear"
 alias cd..="cd .."
+alias claer="clear"
+alias claer="clear"
+alias clare="clear"
+alias cleae="clear"
+alias clera="clear"
 
 #youtube-dl
-alias ytdl="youtube-dl"
 alias mp3="youtube-dl --audio-format mp3 -k"
 alias yta="yt -x -f bestaudio/best"
+alias ytdl="youtube-dl"
 
 #replace root-only commands with sudo [command]
 for command in mount umount sv visudo pacman updatedb su shutdown poweroff reboot ; do
