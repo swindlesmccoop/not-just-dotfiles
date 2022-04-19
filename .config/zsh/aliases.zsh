88a5a1354e6e9a84113c97f590b95b647ca20722
swindlesmccoop <swindlesmccoop@waifu.club>
2022-04-19 04:37:17 -0400

    Clean up
    Former-commit-id: dab29736d48380a3769b42d0e483e63d22c67a1c

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,5 +1,4 @@
-#general aliases
-alias fzfse="fzf --layout=reverse --height 40%"
+#general  aliases
 alias enc="gpg -c --cipher-algo AES256"
 alias ":q"="exit"
 alias c="clear"
@@ -11,6 +10,7 @@ alias nvim="vim"
 alias yt="yt-dlp"
 alias showme="mpv /dev/video0 2&> /dev/null &"
 alias asciime="mplayer tv:// -vo caca"
+alias lf="lfub"
 
 #edit configurations
 alias pacman.conf="sudo vim /etc/pacman.conf"
@@ -20,8 +20,9 @@ alias sudoers="visudo"
 alias vimrc="vim ~/.config/vim/vimrc"
 alias zshrc="vim ~/.config/zsh/.zshrc"
 alias aliases="vim ~/.config/zsh/aliases.zsh"
+alias liisten="vgmstream123 -l 2 -f 5 -m"
 
-#git aliases
+#git  aliases
 alias gacap="git add . && git commit -a && git push"
 
 #spellign mistaeks
